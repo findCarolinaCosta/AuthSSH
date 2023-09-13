377343c7efc4ea28232706389c5cd066c687f8d0
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-09-13 04:38:41 -0300

    feat: spring base API project

---
+++ src/main/java/com/AuthSSH/ssh/AuthSshApplication.java
@@ -0,0 +1,13 @@
+package com.AuthSSH.ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AuthSSH/ssh/AuthSshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthSSH.ssh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
